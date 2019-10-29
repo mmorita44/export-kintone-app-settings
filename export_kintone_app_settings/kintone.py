f008c43b6e6339bbbd734fde8317638c3503130d
Masato Morita <mamorita@m-sol.co.jp>
2019-10-29 17:37:50 +0900

    出力ファイルをJSON化する

--- export_kintone_app_settings/kintone.py
+++ export_kintone_app_settings/kintone.py
@@ -8,7 +8,7 @@ class Kintone:
         authorization = base64.b64encode((login_name + ':' + password).encode('utf-8'))
         self.headers = {'X-Cybozu-Authorization': authorization.decode('utf-8')}
         self.domain = domain
-        self.file_format = 'app_{}_{}.txt'
+        self.file_format = 'app_{}_{}.json'
 
     def export(self, app_ids):
         try:
@@ -16,26 +16,28 @@ class Kintone:
             for app_id in app_ids:
                 filename = self.file_format.format(app_id, datetime.today().strftime("%Y%m%d%H%M%S"))
                 with open(filename, 'w') as file:
+                    file.writelines('{')
                     # アプリのアクセス権
                     resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/acl.json?app={app_id}',
                                        headers=self.headers)
-                    file.writelines(resp.content.decode('utf-8') + '\n')
+                    file.writelines(f'"アプリのアクセス権": {resp.content.decode("utf-8")},')
                     # レコードのアクセス権
                     resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/record/acl.json?app={app_id}',
                                        headers=self.headers)
-                    file.writelines(resp.content.decode('utf-8') + '\n')
+                    file.writelines(f'"レコードのアクセス権": {resp.content.decode("utf-8")},')
                     # フィールドのアクセス権
                     resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/field/acl.json?app={app_id}',
                                        headers=self.headers)
-                    file.writelines(resp.content.decode('utf-8') + '\n')
+                    file.writelines(f'"フィールドのアクセス権": {resp.content.decode("utf-8")},')
                     # JavaScript/CSSカスタマイズ設定
                     resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/customize.json?app={app_id}',
                                        headers=self.headers)
-                    file.writelines(resp.content.decode('utf-8') + '\n')
+                    file.writelines(f'"JavaScript/CSSカスタマイズ設定": {resp.content.decode("utf-8")},')
                     # プロセス管理の設定
                     resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/status.json?app={app_id}',
                                        headers=self.headers)
-                    file.writelines(resp.content.decode('utf-8') + '\n')
+                    file.writelines(f'"プロセス管理の設定": {resp.content.decode("utf-8")}')
+                    file.writelines('}')
             return 1
         except Exception:
             print(traceback.format_exc())
