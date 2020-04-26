d33feb0b2adf362992ceb9c1ab279a0fed029b6b
Masato Morita <m.morita44@hotmail.com>
2020-04-26 12:40:35 +0900

    argparseの適用

--- export_kintone_app_settings/__init__.py
+++ export_kintone_app_settings/__init__.py
@@ -1,9 +1,12 @@
-import sys
+import argparse
 from .kintone import Kintone
 
+
 def main():
-    domain = sys.argv[1]
-    login_name = sys.argv[2]
-    password = sys.argv[3]
-    app_ids = sys.argv[4].split(',')
-    return Kintone(domain, login_name, password).export(app_ids)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("domain", type=str)
+    parser.add_argument("login_name", type=str)
+    parser.add_argument("password", type=str)
+    parser.add_argument("--app", nargs='+', type=int, dest='app')
+    args = parser.parse_args()
+    return Kintone(args.domain, args.login_name, args.password).export(args.app)
