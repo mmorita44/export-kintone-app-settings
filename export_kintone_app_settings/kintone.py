c8883f788cec6abd5db174a58361169ddf83a9eb
Masato Morita <mamorita@m-sol.co.jp>
2021-03-17 13:49:07 +0900

    エラーメッセージの変更

--- export_kintone_app_settings/kintone.py
+++ export_kintone_app_settings/kintone.py
@@ -7,6 +7,8 @@ from requests import Session
 
 
 class Kintone:
+    ERROR_MESSAGE = "[Error] app_id: {}, api_name: {}, status_code: {}, content: {}"
+
     def __init__(self, domain, login_name, password):
         authorization = base64.b64encode(
             (login_name + ':' + password).encode('utf-8'))
@@ -22,117 +24,132 @@ class Kintone:
             session = Session()
             for app_id in app_ids:
                 file_contents = {}
+                api_name = ""
                 # 一般設定
+                api_name = "一般設定"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/settings.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["一般設定"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # フィールドの一覧
+                api_name = "フィールドの一覧"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/form/fields.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["フィールドの一覧"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # フォームのレイアウト
+                api_name = "フォームのレイアウト"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/form/layout.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["フォームのレイアウト"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # 一覧の設定
+                api_name = "一覧の設定"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/views.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["一覧の設定"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # アプリのアクセス権
+                api_name = "アプリのアクセス権"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/acl.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["アプリのアクセス権"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # レコードのアクセス権
+                api_name = "レコードのアクセス権"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/record/acl.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["レコードのアクセス権"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # フィールドのアクセス権
+                api_name = "フィールドのアクセス権"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/field/acl.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["フィールドのアクセス権"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # JavaScript/CSSカスタマイズ設定
+                api_name = "JavaScript/CSSカスタマイズ設定"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/customize.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["JavaScript/CSSカスタマイズ設定"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # プロセス管理の設定
+                api_name = "プロセス管理の設定"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/status.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["プロセス管理の設定"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # アプリの条件通知
+                api_name = "アプリの条件通知"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/notifications/general.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["アプリの条件通知"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # レコードの条件通知
+                api_name = "レコードの条件通知"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/notifications/perRecord.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["レコードの条件通知"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # リマインダーの条件通知
+                api_name = "リマインダーの条件通知"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/notifications/reminder.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["リマインダーの条件通知"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # アプリのグラフ設定の取得
+                api_name = "アプリのグラフ設定の取得"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/reports.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["アプリのグラフ設定の取得"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
                 # アプリのアクションの設定の取得
+                api_name = "アプリのアクションの設定の取得"
                 resp = session.get(f'https://{self.domain}.cybozu.com/k/v1/app/actions.json?app={app_id}',
                                    headers=self.headers)
                 if resp.status_code != 200:
-                    raise Exception('[Error] app_id: {}, status_code: {}, content: {}'
-                                    .format(app_id, resp.status_code, resp.content.decode("utf-8")))
-                file_contents["アプリのアクションの設定の取得"] = json.loads(
+                    raise Exception(self.ERROR_MESSAGE.format(
+                        app_id, api_name, resp.status_code, resp.content.decode("utf-8")))
+                file_contents[api_name] = json.loads(
                     resp.content.decode("utf-8"))
 
                 filename = self.file_format.format(app_id)
